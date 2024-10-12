2e5a99bac474aacdeab644aa4f604e4e22a42cc1
Vishal Pal <118637617+Huge1729@users.noreply.github.com>
2024-10-12 23:08:09 +0530

    Update and rename 03_Advance_Algorithim to 03_Advance_Algorithim.cpp

---
+++ Graphs/Concepts/03_Advance_Algorithim.cpp
@@ -0,0 +1,159 @@
+************************ Kosaraju’s Algorithm for strongly connected component
+class Solution
+{
+private:
+    void dfs(int node, vector<int> &vis, vector<int> adj[],
+             stack<int> &st) {
+        vis[node] = 1;
+        for (auto it : adj[node]) {
+            if (!vis[it]) {
+                dfs(it, vis, adj, st);
+            }
+        }
+
+        st.push(node);
+    }
+private:
+    void dfs3(int node, vector<int> &vis, vector<int> adjT[]) {
+        vis[node] = 1;
+        for (auto it : adjT[node]) {
+            if (!vis[it]) {
+                dfs3(it, vis, adjT);
+            }
+        }
+    }
+public:
+    //Function to find number of strongly connected components in the graph.
+    int kosaraju(int V, vector<int> adj[])
+    {
+        vector<int> vis(V, 0);
+        stack<int> st;
+        for (int i = 0; i < V; i++) {
+            if (!vis[i]) {
+                dfs(i, vis, adj, st);
+            }
+        }
+
+        vector<int> adjT[V];
+        for (int i = 0; i < V; i++) {
+            vis[i] = 0;
+            for (auto it : adj[i]) {
+                // i -> it
+                // it -> i
+                adjT[it].push_back(i);
+            }
+        }
+        int scc = 0;
+        while (!st.empty()) {
+            int node = st.top();
+            st.pop();
+            if (!vis[node]) {
+                scc++;
+                dfs3(node, vis, adjT);
+            }
+        }
+        return scc;
+    }
+};
+
+************************************ Trajoin's Algorithim for Bridges
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    int timer = 1;
+    void dfs(int node, int parent, vector<int> &vis,
+             vector<int> adj[], int tin[], int low[], vector<vector<int>> &bridges) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+            if (vis[it] == 0) {
+                dfs(it, node, vis, adj, tin, low, bridges);
+                low[node] = min(low[it], low[node]);
+                // node --- it
+                if (low[it] > tin[node]) {
+                    bridges.push_back({it, node});
+                }
+            }
+            else {
+                low[node] = min(low[node], low[it]);
+            }
+        }
+    }
+public:
+    vector<vector<int>> criticalConnections(int n,
+    vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for (auto it : connections) {
+            int u = it[0], v = it[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<vector<int>> bridges;
+        dfs(0, -1, vis, adj, tin, low, bridges);
+        return bridges;
+    }
+};
+
+
+**************************************************** Articultion Points ***********************************
+#include <bits/stdc++.h>
+using namespace std;
+
+//User function Template for C++
+
+class Solution {
+private:
+    int timer = 1;
+    void dfs(int node, int parent, vector<int> &vis, int tin[], int low[],
+             vector<int> &mark, vector<int>adj[]) {
+        vis[node] = 1;
+        tin[node] = low[node] = timer;
+        timer++;
+        int child = 0;
+        for (auto it : adj[node]) {
+            if (it == parent) continue;
+            if (!vis[it]) {
+                dfs(it, node, vis, tin, low, mark, adj);
+                low[node] = min(low[node], low[it]);
+                if (low[it] >= tin[node] && parent != -1) {
+                    mark[node] = 1;
+                }
+                child++;
+            }
+            else {
+                low[node] = min(low[node], tin[it]);
+            }
+        }
+        if (child > 1 && parent == -1) {
+            mark[node] = 1;
+        }
+    }
+public:
+    vector<int> articulationPoints(int n, vector<int>adj[]) {
+        vector<int> vis(n, 0);
+        int tin[n];
+        int low[n];
+        vector<int> mark(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                dfs(i, -1, vis, tin, low, mark, adj);
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < n; i++) {
+            if (mark[i] == 1) {
+                ans.push_back(i);
+            }
+        }
+        if (ans.size() == 0) return { -1};
+        return ans;
+    }
+};
